8644eb83a3325e63688eb87df86cd1a3958b12be
Sufi <matemarcalek@gmail.com>
2023-11-05 17:35:13 +0100

    Add project files.

---
+++ Hazi231105/Program.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections.Generic;
+
+namespace Hazi231105
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Szoftver> szoftverek = new();
+            using StreamReader sr = new(
+                path: @"..\..\..\src\szoftverek.txt",
+                encoding: System.Text.Encoding.UTF8);
+            _ = sr.ReadLine();
+            while (!sr.EndOfStream) szoftverek.Add(new(sr.ReadLine()));
+            foreach(var sz in szoftverek)
+            {
+                Console.WriteLine(sz.ToString());
+            }
+            Console.WriteLine("7. feladat:");
+            var f7 = szoftverek.Where(sz => sz.Kategoria.Equals("antivírus") && sz.Ertekeles >= 8.5);
+            Console.WriteLine($"\t{f7.Count()} db 8,5-nél magasabb értélelésű antivírus szoftver van");
+
+            Console.WriteLine("8. feladat:");
+            double max = szoftverek.Max(sz => sz.Ertekeles);
+            var f8 = szoftverek.Where(sz => sz.Ertekeles.Equals(max - 0.1));
+            foreach (var f in f8)
+            {
+                Console.WriteLine(f.ToString());
+            }
+            Console.WriteLine($"A keresett átlag:  {max - 0.1}");
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Hazi231105/Szoftver.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Hazi231105
+{
+    internal class Szoftver
+    {
+        public int Sorszam { get; set; }
+        public string Nev {  get; set; }
+        public string LicenszType { get; set; }
+        public string[] OPrendszerek { get; set; }
+        public string Kategoria {  get; set; }
+        public double Ertekeles {  get; set; }
+        public double Ar {  get; set; }
+        public int AdoKulcs {  get; set; }
+
+        public Szoftver(string sor) 
+        {
+            var v = sor.Split('|');
+            Sorszam = int.Parse(v[0]);
+            Nev = v[1];
+            LicenszType = v[2];
+            OPrendszerek = v[3].Split(',');
+            Kategoria = v[4];
+            Ertekeles = double.Parse(v[5]);
+            Ar = double.Parse(v[6]);
+            AdoKulcs = int.Parse(v[7]);
+        }
+
+        public override string ToString()
+        {
+            return $"Sorszam:  {Sorszam},\nNev:  {Nev},\nLicensz típus:  {LicenszType},\nOperációs rendszerek:  {OPrendszerek},\nÉrtékelés:  {Ertekeles},\nÁr:  {Ar},\nAdókulcs:  {AdoKulcs}";
+
+        }
+    }
+}
